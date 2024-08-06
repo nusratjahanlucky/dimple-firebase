7f50ee7d68bc2836e9c0a9989cf6a69366f1c8aa
Nusrat Jahan Lucky <nusratjahanlucky00@gmail.com>
2024-08-06 21:26:44 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlVLbwp26AO6i0FOCoO0GyumdFJzfqUno",
+  authDomain: "dimple-firebase-5c165.firebaseapp.com",
+  projectId: "dimple-firebase-5c165",
+  storageBucket: "dimple-firebase-5c165.appspot.com",
+  messagingSenderId: "666061539859",
+  appId: "1:666061539859:web:7837e4607afef17f4c4de6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
